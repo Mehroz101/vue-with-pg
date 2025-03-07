1e8d32bc24748d5c159ba75e552f58996183980d
Mehroz <mehrozfaooq127@gmail.com>
2025-03-07 15:42:59 +0500

    tranfer files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,33 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
 
+
+
+import express from "express";
+import cors from "cors";
+import User from "./models/user";
+import bcrypt from "bcrypt";
 const app = express();
 app.use(cors());
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
-app.listen(4000, () => {
-  console.log("Server is running on port 3000");
+app.use(express.json())
+app.post("/api/register",async (req, res) => {
+    try {
+        const { fullname, email, password } = req.body;
+        
+        // Hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+    
+        // Store in DB
+        const users = await User.create({ name:fullname, email, password: hashedPassword });
+    
+        res.status(201).json(users);
+      } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: error.message });
+      }
 })
-// Export the Express app to be used as a Vercel serverless function
-export default app;
+app.get("/", (req, res) => {    
+    res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
