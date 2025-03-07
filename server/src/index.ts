df337aa3a6986e1b647548e7c2b951e9820a9667
Mehroz <mehrozfaooq127@gmail.com>
2025-03-07 15:10:21 +0500

    check

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,12 @@
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
 
-const express = require("express");
 const app = express();
-const cors = require("cors");
-
 app.use(cors());
 
-app.get("/", (req:Request, res:Response) => {
-    res.send("Hello World!");
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+// Export the Express app to be used as a Vercel serverless function
+export default app;
